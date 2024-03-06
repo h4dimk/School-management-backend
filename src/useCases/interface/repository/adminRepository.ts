62dc94c99d74ba57ca6cc4f5939ff9a5823f76dd
Hadi mk <hadimk04@gmail.com>
2024-03-06 13:11:06 +0530

    partial work completed

--- src/useCases/interface/repository/adminRepository.ts
+++ src/useCases/interface/repository/adminRepository.ts
@@ -2,5 +2,5 @@ import { IAdmin } from "../../../entities/adminEntity";
 
 export interface IAdminRepository {
   findByEmail(email: string): Promise<IAdmin | null>;
-  create(admin: IAdmin): Promise<void>;
+  create(admin: IAdmin): Promise<void>; 
 }
